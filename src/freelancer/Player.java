3d812736ecdb1688542a6f283f8022a8ce7322c0
foobles <buzdav2@gmail.com>
2021-06-04 14:25:14 -0500

    added evidence record

--- src/freelancer/Player.java
+++ src/freelancer/Player.java
@@ -1,4 +1,36 @@
 package freelancer;
 
-public class Player {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public final class Player {
+    private final ArrayList<Evidence> evidence;
+
+    public Player() {
+        evidence = new ArrayList<>();
+    }
+
+    public Player(ArrayList<Evidence> evidence) {
+        this.evidence = evidence;
+    }
+
+    public List<Prompt<Evidence>> getEvidencePrompts(Evidence.Sort sort) {
+        return evidence.stream()
+                .filter(e -> e.getSort() == sort)
+                .map(e -> new Prompt<>(e.getName(), e))
+                .collect(Collectors.toList());
+    }
+
+    public Optional<Evidence> getEvidence(Object id) {
+        return evidence.stream().filter(e -> e.getID() == id).findAny();
+    }
+
+    public void addEvidence(Evidence toAdd) {
+        if (evidence.stream().anyMatch(e -> e.getID() == toAdd.getID())) {
+            throw new RuntimeException("Cannot add evidence with duplicate ids (" + toAdd.getID() + ")");
+        }
+        evidence.add(toAdd);
+    }
 }
